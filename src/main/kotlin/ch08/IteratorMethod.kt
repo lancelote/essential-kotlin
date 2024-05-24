8e0df307ee29a4d1111bb78c1e75035c32a5f2de
Pavel Karateev <lancelote.du.lac@gmail.com>
2024-05-24 22:35:10 +0200

    `iterator` method example

---
+++ src/main/kotlin/ch08/IteratorMethod.kt
@@ -0,0 +1,25 @@
+package ch08
+
+class Tree(
+    val value: String,
+    val left: Tree? = null,
+    val right: Tree? = null,
+) : Iterable<String> {
+    override fun iterator(): Iterator<String> = iterator {
+        if (left != null) yieldAll(left)
+        yield(value)
+        if (right != null) yieldAll(right)
+    }
+}
+
+fun main() {
+    val tree = Tree(
+        value = "B",
+        left = Tree("A"),
+        right = Tree("D", left = Tree("C"))
+    )
+
+    for (value in tree) {
+        print(value)
+    }
+}
