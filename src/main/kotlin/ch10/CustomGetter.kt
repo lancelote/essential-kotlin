c4a8854a74e87397fe1d86c45fd3f66618e53b99
Pavel Karateev <lancelote.du.lac@gmail.com>
2024-05-26 13:10:21 +0200

    Custom setter example

--- src/main/kotlin/ch10/CustomGetter.kt
+++ src/main/kotlin/ch10/CustomGetter.kt
@@ -3,10 +3,16 @@ package ch10
 class User {
     var name: String = ""
         get() = field.uppercase()
+        set(value) {
+            if (value.isNotBlank()) {
+                field = value
+            }
+        }
 }
 
 fun main() {
     val user = User()
     user.name = "Norbert"
+    user.name = ""
     println(user.name)
 }
